ed09687d7aaad828353639651a95e6077846476f
Volodymyr <vgerun97@gmail.com>
2024-07-07 20:30:24 +0300

    feat: add tests with vitest (#4)
    * feat: add tests with vitest
    
    * ci: add testing job
    
    * chore(ci): change job names

---
+++ __tests__/functions/capitalizeFirstLetter.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from 'vitest'
+
+import { capitalizeFirstLetter } from '../../app/lib/utils'
+
+describe('capitalizeFirstLetter', () => {
+  it('should capitalize the first letter of a string', () => {
+    expect(capitalizeFirstLetter('hello')).toBe('Hello')
+  })
+
+  it('should return an empty string if input is empty', () => {
+    expect(capitalizeFirstLetter('')).toBe('')
+  })
+
+  it('should return the same string if the first character is already capitalized', () => {
+    expect(capitalizeFirstLetter('Hello')).toBe('Hello')
+  })
+
+  it('should handle single character strings', () => {
+    expect(capitalizeFirstLetter('h')).toBe('H')
+  })
+
+  it('should return the same string if input is a non-letter character', () => {
+    expect(capitalizeFirstLetter('1hello')).toBe('1hello')
+    expect(capitalizeFirstLetter('-hello')).toBe('-hello')
+  })
+})
---
+++ app/vitest-test/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>vitest test</p>
+    </div>
+  )
+}
