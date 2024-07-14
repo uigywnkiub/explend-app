6ed31a65772570179456efefcebd9ad4e80c6b67
Volodymyr <vgerun97@gmail.com>
2024-07-14 12:36:50 +0300

    chore: add lint-staged (#16)
    * chore: add lint-staged
    
    * style: prettier:fix
    
    * chore: update lint-staged config

---
+++ .lintstagedrc.js
@@ -0,0 +1,13 @@
+import { relative } from 'path'
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => relative(process.cwd(), f))
+    .join(' --file ')}`
+
+const config = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '**/*': ['yarn prettier:fix'],
+}
+
+export default config
