e8a61975b320500909a2836a1b3ce236e29a5d75
Volodymyr Gerun <vgerun97@gmail.com>
2024-07-31 13:37:53 +0300

    style: add props double asterisk

--- app/ui/info-badge.tsx
+++ app/ui/info-badge.tsx
@@ -1,12 +1,18 @@
 type TProps = {
   text: string
   withAsterisk?: boolean
+  withDoubleAsterisk?: boolean
 }
 
-export default function InfoBadge({ text, withAsterisk = true }: TProps) {
+export default function InfoBadge({
+  text,
+  withAsterisk = true,
+  withDoubleAsterisk = false,
+}: TProps) {
   return (
     <span className='text-xxs text-default-300 md:text-xs'>
-      {withAsterisk && ' * '}
+      {withDoubleAsterisk && ' ** '}
+      {!withDoubleAsterisk && withAsterisk && ' * '}
       {text}
     </span>
   )
