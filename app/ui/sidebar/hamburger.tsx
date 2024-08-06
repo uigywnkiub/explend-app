6b49bc0e583b7c528aa1035ff5c523f1e55de6a5
Volodymyr Gerun <vgerun97@gmail.com>
2024-08-06 12:58:13 +0300

    feat: add eslint-plugin-jsx-a11y

--- app/ui/sidebar/hamburger.tsx
+++ app/ui/sidebar/hamburger.tsx
@@ -1,9 +1,7 @@
 export default function Hamburger() {
   return (
     <label
-      tabIndex={1}
       aria-label='Open Menu'
-      role='button'
       htmlFor='sidebar-toggle'
       className='label fixed right-[16px] top-[16px] z-50 cursor-pointer rounded-md bg-background/80 px-[3px] py-[8px] backdrop-blur-3xl md:right-[32px] md:top-[32px] md:hidden'
     >
