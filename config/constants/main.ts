9396700ec663d4f84f357a0c73c2cb8453525622
Volodymyr Gerun <vgerun97@gmail.com>
2024-08-15 12:19:15 +0300

    chore: implement beforeSend sentry handler

--- config/constants/main.ts
+++ config/constants/main.ts
@@ -32,5 +32,8 @@ export const APP_DESCRIPTION: string =
 export const APP_URL: string = process.env.APP_URL
 export const APP_LOCALHOST_URL: string = process.env.APP_LOCALHOST_URL
 // This regex will match any URL that starts with https://explend-, followed by any alphanumeric and hyphenated string, and ends with -projects.vercel.app.
-export const REGEX_APP_PREVIEW_PROD_URL: RegExp =
-  /^https:\/\/explend-[\w\d-]+-projects[\w\d-]*\.vercel\.app(\/.*)?$/
+// export const REGEX_APP_PREVIEW_PROD_URL: RegExp =
+//   /^https:\/\/explend-[\w\d-]+-projects[\w\d-]*\.vercel\.app(\/.*)?$/
+
+export const SENTRY_WORKER_ERROR_TEXT =
+  'SecurityError: Failed to register a ServiceWorker'
--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,36 +3,17 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 import * as Sentry from '@sentry/nextjs'
 
-import {
-  APP_URL,
-  IS_PROD,
-  REGEX_APP_PREVIEW_PROD_URL,
-} from './config/constants/main'
-import { ROUTE } from './config/constants/routes'
+import { IS_PROD, SENTRY_WORKER_ERROR_TEXT } from './config/constants/main'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-  // Enable Sentry only in production mode
-  enabled: true,
-
-  // denyUrls: [
-  //   REGEX_APP_PREVIEW_PROD_URL,
-  //   APP_URL + '/sw.js',
-  //   APP_URL + ROUTE.SIGNIN,
-  // ],
+  enabled: IS_PROD,
 
   // @ts-ignore
-  beforeSend(event, hint) {
-    console.log('event', event)
-    console.log('hint', hint)
-
-    if (
-      event.message &&
-      event.message.startsWith(
-        'SecurityError: Failed to register a ServiceWorker',
-      )
-    ) {
+  beforeSend(event) {
+    // Skip error if the user has no authentication because on the sign-in page service worker can’t run.
+    if (event.message && event.message.startsWith(SENTRY_WORKER_ERROR_TEXT)) {
       return null
     }
 
