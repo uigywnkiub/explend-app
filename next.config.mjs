0c588b3d71e1ec4a6814923f631bb798fd1f55c5
Volodymyr Gerun <vgerun97@gmail.com>
2024-08-27 12:57:43 +0300

    chore(sentry): enable widenClientFileUpload

--- next.config.mjs
+++ next.config.mjs
@@ -84,7 +84,7 @@ const sentryConfig = {
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
   // Upload a larger set of source maps for prettier stack traces (increases build time).
-  widenClientFileUpload: false,
+  widenClientFileUpload: true,
 
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
