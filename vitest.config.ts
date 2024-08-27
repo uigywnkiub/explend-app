cc9bd57acbb625a4b6218b1e70cae534a566ac4e
Volodymyr Gerun <vgerun97@gmail.com>
2024-08-27 15:16:06 +0300

    chore: upgrade deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { defineConfig } from 'vitest/config'
+import { defineConfig, UserConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()] as UserConfig['plugins'],
   test: {
     environment: 'jsdom',
   },
