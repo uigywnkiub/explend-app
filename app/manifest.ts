201f74cc18a5ce711620fba968f0d319d54b3f70
Volodymyr Gerun <vgerun97@gmail.com>
2024-08-27 20:57:30 +0300

    refactor: use const variables for screenshots key in manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -10,6 +10,16 @@ import {
 import { NAV_TITLE } from '@/config/constants/navigation'
 
 export default function manifest(): MetadataRoute.Manifest {
+  // Desktop.
+  const SCREENSHOT_SIZE_DESKTOP = '2840x1560'
+  const SCREENSHOT_FORM_FACTOR_DESKTOP = 'wide'
+  // Mobile.
+  const SCREENSHOT_SIZE_MOBILE = '1125x2323'
+  const SCREENSHOT_FORM_FACTOR_MOBILE = 'narrow'
+  const SCREENSHOT_MOBILE_SUFFIX = 'mobile'
+  // Desktop and mobile.
+  const SCREENSHOT_TYPE = 'image/png'
+
   return {
     name: APP_NAME.SHORT,
     short_name: APP_NAME.SHORT,
@@ -44,51 +54,51 @@ export default function manifest(): MetadataRoute.Manifest {
     screenshots: [
       {
         src: '/images/screenshots/home.png',
-        sizes: '2840x1560',
-        type: 'image/png',
+        sizes: SCREENSHOT_SIZE_DESKTOP,
+        type: SCREENSHOT_TYPE,
         // @ts-ignore
-        form_factor: 'wide',
+        form_factor: SCREENSHOT_FORM_FACTOR_DESKTOP,
         label: NAV_TITLE.HOME,
       },
       {
         src: '/images/screenshots/monthly-report.png',
-        sizes: '2840x1560',
-        type: 'image/png',
+        sizes: SCREENSHOT_SIZE_DESKTOP,
+        type: SCREENSHOT_TYPE,
         // @ts-ignore
-        form_factor: 'wide',
+        form_factor: SCREENSHOT_FORM_FACTOR_DESKTOP,
         label: NAV_TITLE.MONTHLY_REPORT,
       },
       {
         src: '/images/screenshots/chart.png',
-        sizes: '2840x1560',
-        type: 'image/png',
+        sizes: SCREENSHOT_SIZE_DESKTOP,
+        type: SCREENSHOT_TYPE,
         // @ts-ignore
-        form_factor: 'wide',
+        form_factor: SCREENSHOT_FORM_FACTOR_DESKTOP,
         label: NAV_TITLE.CHART,
       },
       {
         src: '/images/screenshots/home-mobile.png',
-        sizes: '1125x2323',
-        type: 'image/png',
+        sizes: SCREENSHOT_SIZE_MOBILE,
+        type: SCREENSHOT_TYPE,
         // @ts-ignore
-        form_factor: 'narrow',
-        label: `${NAV_TITLE.HOME} mobile`,
+        form_factor: SCREENSHOT_FORM_FACTOR_MOBILE,
+        label: `${NAV_TITLE.HOME} ${SCREENSHOT_MOBILE_SUFFIX}`,
       },
       {
         src: '/images/screenshots/monthly-report-mobile.png',
-        sizes: '1125x2323',
-        type: 'image/png',
+        sizes: SCREENSHOT_SIZE_MOBILE,
+        type: SCREENSHOT_TYPE,
         // @ts-ignore
-        form_factor: 'narrow',
-        label: `${NAV_TITLE.MONTHLY_REPORT} mobile`,
+        form_factor: SCREENSHOT_FORM_FACTOR_MOBILE,
+        label: `${NAV_TITLE.MONTHLY_REPORT} ${SCREENSHOT_MOBILE_SUFFIX}`,
       },
       {
         src: '/images/screenshots/categories-mobile.png',
-        sizes: '1125x2323',
-        type: 'image/png',
+        sizes: SCREENSHOT_SIZE_MOBILE,
+        type: SCREENSHOT_TYPE,
         // @ts-ignore
-        form_factor: 'narrow',
-        label: `${NAV_TITLE.CATEGORIES} mobile`,
+        form_factor: SCREENSHOT_FORM_FACTOR_MOBILE,
+        label: `${NAV_TITLE.CATEGORIES} ${SCREENSHOT_MOBILE_SUFFIX}`,
       },
     ],
   }
