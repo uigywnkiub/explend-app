f5cc1c94a8465babf724d4a679211e13dfc18eab
uigywnkiub <vgerun97@gmail.com>
2025-11-28 20:08:47 +0200

    chore: run prettier

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -25,69 +25,67 @@ export async function GET(req: Request) {
   ).then((res) => res.arrayBuffer())
 
   return new ImageResponse(
-    (
+    <div
+      style={{
+        display: 'flex',
+        height: '100%',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        letterSpacing: '-.02em',
+        fontFamily: fontFamilyName,
+        background: CUSTOM_LIGHT,
+        // backgroundImage: `radial-gradient(circle at 25px 25px, ${CUSTOM_DARK}${OPACITY.O30} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${CUSTOM_DARK}${OPACITY.O30} 2%, transparent 0%)`,
+        // backgroundSize: '100px 100px',
+      }}
+    >
+      <div
+        style={{
+          left: 42,
+          top: 42,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <span
+          style={{
+            width: 24,
+            height: 24,
+            background: `linear-gradient(to bottom, ${SUCCESS}, ${DANGER})`,
+            borderRadius: 6,
+          }}
+        />
+        <span
+          style={{
+            marginLeft: 8,
+            fontSize: 20,
+          }}
+        >
+          {APP_NAME.FULL.toLowerCase()}
+        </span>
+      </div>
       <div
         style={{
           display: 'flex',
-          height: '100%',
-          width: '100%',
-          alignItems: 'center',
+          flexWrap: 'wrap',
           justifyContent: 'center',
-          letterSpacing: '-.02em',
-          fontFamily: fontFamilyName,
-          background: CUSTOM_LIGHT,
-          // backgroundImage: `radial-gradient(circle at 25px 25px, ${CUSTOM_DARK}${OPACITY.O30} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${CUSTOM_DARK}${OPACITY.O30} 2%, transparent 0%)`,
-          // backgroundSize: '100px 100px',
+          padding: '30px 60px',
+          margin: '0 42px',
+          textWrap: 'balance',
+          fontSize: 44,
+          width: 'auto',
+          maxWidth: 640,
+          textAlign: 'center',
+          backgroundColor: CUSTOM_DARK,
+          color: CUSTOM_LIGHT,
+          lineHeight: 1.4,
+          borderRadius: 24,
         }}
       >
-        <div
-          style={{
-            left: 42,
-            top: 42,
-            position: 'absolute',
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          <span
-            style={{
-              width: 24,
-              height: 24,
-              background: `linear-gradient(to bottom, ${SUCCESS}, ${DANGER})`,
-              borderRadius: 6,
-            }}
-          />
-          <span
-            style={{
-              marginLeft: 8,
-              fontSize: 20,
-            }}
-          >
-            {APP_NAME.FULL.toLowerCase()}
-          </span>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            padding: '30px 60px',
-            margin: '0 42px',
-            textWrap: 'balance',
-            fontSize: 44,
-            width: 'auto',
-            maxWidth: 640,
-            textAlign: 'center',
-            backgroundColor: CUSTOM_DARK,
-            color: CUSTOM_LIGHT,
-            lineHeight: 1.4,
-            borderRadius: 24,
-          }}
-        >
-          {title}
-        </div>
+        {title}
       </div>
-    ),
+    </div>,
     // ImageResponse options.
     {
       width: 1200,
