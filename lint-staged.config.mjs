20ada97b6ac4a95120cde1bd02d5dbe3b85cd96d
Volodymyr <vgerun97@gmail.com>
2026-02-07 17:50:41 +0200

    chore: upgrade nextjs from v15 to v16 via next-devtools-mcp@latest (#221)
    * chore: upgrade Next.js 15 to 16, React 19.2, types
    
    * chore: upgrade nextjs from v15 to v16 via next-devtools-mcp@latest
    
    * fix: rm overrides in package.json

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 const config = {
   '*.{js,jsx,mjs,cjs,ts,tsx,md,html}': ['eslint --fix'],
-  '**/*': ['prettier --write'],
+  '**/*': ['prettier --write --ignore-unknown'],
 }
 
 export default config
