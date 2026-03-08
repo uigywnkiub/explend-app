2c10b97f7a2bc445843a07d82b846e932f909b06
uigywnkiub <vgerun97@gmail.com>
2026-03-08 11:16:44 +0200

    feat: add test apple splash images

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,9 +52,88 @@ export const metadata: Metadata = {
     capable: true,
     statusBarStyle: 'black-translucent',
     title: APP_NAME.SHORT,
-    startupImage: {
-      url: '/icons/apple-touch-icon.png',
-    },
+    startupImage: [
+      {
+        url: '/apple-splash-1320-2868.jpg',
+        media:
+          '(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: '/apple-splash-2868-1320.jpg',
+        media:
+          '(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)',
+      },
+      {
+        url: '/apple-splash-1488-2266.jpg',
+        media:
+          '(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: '/apple-splash-2266-1488.jpg',
+        media:
+          '(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)',
+      },
+      {
+        url: '/apple-splash-1536-2048.jpg',
+        media:
+          '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: '/apple-splash-2048-1536.jpg',
+        media:
+          '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
+      },
+      {
+        url: '/apple-splash-1620-2160.jpg',
+        media:
+          '(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: '/apple-splash-2160-1620.jpg',
+        media:
+          '(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
+      },
+      {
+        url: '/apple-splash-1640-2360.jpg',
+        media:
+          '(device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: '/apple-splash-2360-1640.jpg',
+        media:
+          '(device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
+      },
+      {
+        url: '/apple-splash-1668-2224.jpg',
+        media:
+          '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: '/apple-splash-2224-1668.jpg',
+        media:
+          '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
+      },
+      {
+        url: '/apple-splash-1668-2388.jpg',
+        media:
+          '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: '/apple-splash-2388-1668.jpg',
+        media:
+          '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
+      },
+      {
+        url: '/apple-splash-2048-2732.jpg',
+        media:
+          '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: '/apple-splash-2732-2048.jpg',
+        media:
+          '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
+      },
+    ],
   },
   robots: {
     index: true,
@@ -80,6 +159,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   themeColor: CUSTOM_DARK_COLOR,
+  maximumScale: 1,
+  userScalable: false,
 }
 
 export default function RootLayout({
